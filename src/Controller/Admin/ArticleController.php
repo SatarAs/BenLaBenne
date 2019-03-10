df34ae8887911babeec558ba3e89565b76b1b0af
Romainrev <32880241+Romainrev@users.noreply.github.com>
2019-03-10 17:10:04 +0100

    Modification ArticleController

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -4,23 +4,25 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+/*
 class ArticleController extends AbstractController
 {
 
     /**
      * @Route("/admin/articles/add", name="admin_article_add")
      */
+/*
     public function add()
     {
         return $this->render('Admin/Add/addArticle.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
-
+/*
     /**
      * @Route("/admin/articles/edit/{id}", name="admin_article_edit")
      */
+/*
     public function edit()
     {
         return $this->render('Admin/edit/editArticle.html.twig', [
@@ -31,6 +33,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/admin/articles/delete/{id}", name="admin_article_delete")
      */
+/*
     public function delete()
     {
         return $this->render('Admin/delete/deleteArticle.html.twig', [
@@ -41,6 +44,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/admin/articles", name="admin_articles_manage")
      */
+/*
     public function articlesManage()
     {
         return $this->render('Admin/showArticles.html.twig', [
