3b5e634cc1ef8d677c89c55e0d8e4b5e406abf67
Romainrev <32880241+Romainrev@users.noreply.github.com>
2019-03-10 17:43:25 +0100

    Modification de ArticleController avec Forms + modifications sur le twig addArticle

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -1,55 +1,63 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\dailySpace;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
 use Symfony\Component\Routing\Annotation\Route;
-/*
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
 class ArticleController extends AbstractController
 {
-
     /**
-     * @Route("/admin/articles/add", name="admin_article_add")
+     * @param Request $request
+     * @return mixed
+     * @Route("/admin/articles/add" name="admin_articles_add")
      */
-/*
-    public function add()
-    {
-        return $this->render('Admin/Add/addArticle.html.twig', [
-            'controller_name' => 'ArticleController',
+    public function addarticle(Request $request) {
+
+        $article = new Article();
+        $form = $this->createFormbuilder($article)
+
+            ->add('titre', TextType::class, [
+                'required'      => true,
+                'label'         => false,
+                'attr'          => [
+                    'class'         =>  'form-control',
+                    'placeholder'   =>  'Titre de l\'Article...'
+                ]
+            ])
+
+            ->add('contenu', TextareaType::class, [
+                'required'      => true,
+                'label'         => false,
+                'attr'          => [
+                    'class'         =>  'form-control',
+                    'placeholder'   =>  'Contenu de l\'Article...'
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'required'      => true,
+                'label'         => false,
+                'attr'          => [
+                    'class'         =>  'dropify',
+                ]
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Publier',
+                'attr'      => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+            ->getForm();
+        $form->handlerequest($request);
+        return $this->render('Article/addArticle.html.twig', [
+            'form' => $form->createView()
         ]);
     }
-/*
-    /**
-     * @Route("/admin/articles/edit/{id}", name="admin_article_edit")
-     */
-/*
-    public function edit()
-    {
-        return $this->render('Admin/edit/editArticle.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
-    }
-
-    /**
-     * @Route("/admin/articles/delete/{id}", name="admin_article_delete")
-     */
-/*
-    public function delete()
-    {
-        return $this->render('Admin/delete/deleteArticle.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
-    }
-
-    /**
-     * @Route("/admin/articles", name="admin_articles_manage")
-     */
-/*
-    public function articlesManage()
-    {
-        return $this->render('Admin/showArticles.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
-    }
-
 }
