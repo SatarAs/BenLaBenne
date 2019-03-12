06f8823a8699b32b9362b624a639cccf646f3530
SatarAs <satar09@hotmail.fr>
2019-03-12 22:31:44 +0100

    Modifications du Controller Admin et ajouts des différents tableaux en vue - Containers

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -12,6 +13,13 @@ class AdminController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents('../public/Json/benne.json');
+
+        $json = substr($json,11);
+        $showContainers = json_decode($json,true);
+        var_dump($showContainers[0]['features'][0]['geometry']);
+
+
         return $this->render('Admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
@@ -42,9 +50,31 @@ class AdminController extends AbstractController
      */
     public function containers()
     {
-        return $this->render('Admin/containers.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        $json = file_get_contents('../public/Json/ben.js');
+
+        $json = substr($json,10);
+        $showContainers = json_decode($json,true);
+
+        // Déclaration des différents tableaux
+        $properties = $showContainers[0]['features'][0]['properties'];
+         // var_dump($showContainers[0]['features']);
+
+        return $this->render('Admin/containers.html.twig', array(
+            'json' => $showContainers,
+            'features' => $showContainers[0]['features'],
+            'properties' => $showContainers[0]['features'][0]['properties'],
+            'coordinatesX' => $showContainers[0]['features'][0]['geometry']['coordinates'][0],
+            'coordinatesY' => $showContainers[0]['features'][0]['geometry']['coordinates'][1],
+
+            // Première manière
+            // 'commune' => $showContainers[0]['features'][0]['properties'],
+
+            // Deuxième manière
+            'commune'           => $properties['commune'],
+            'code_postal'       => $properties['code_postal'],
+            'numerodansvoie'    => $properties['numerodansvoie'],
+            'voie'              => $properties['voie'],
+        ));
     }
 
     /**
