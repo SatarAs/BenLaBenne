c6127d213d1ad92b6def1f39f157489dc0dcb46a
Romainrev <32880241+Romainrev@users.noreply.github.com>
2019-03-17 13:25:59 +0100

    Création Repository Security + creation UsersProviders + GoogleAuthenticator

---
+++ Security2/GoogleAuthenticator.php
@@ -0,0 +1,16 @@
+<?php
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use League\OAuth2\Client\Provider\GoogleUser;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+
+abstract class GoogleAuthenticator extends SocialAuthenticator
+{
+
+}
