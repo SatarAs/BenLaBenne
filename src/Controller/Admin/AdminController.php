e89975284e2bf868f25219d2a734c4e7e2245b31
SatarAs09 <satar09@hotmail.fr>
2019-03-18 02:10:32 +0100

    Ajout du nombre d'articles total - Panel admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class AdminController extends AbstractController
      */
     public function index()
     {
+        $articles       = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
+        $nbrArticles    = count($articles);
+
         return $this->render('Admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'nbrArticles' => $nbrArticles
         ]);
     }
 
